024759754c7ed9a728811addce2bbca463c6f64d
ayenyeinchanpyae <57614389+ayenyeinchanpyae@users.noreply.github.com>
2023-01-03 17:23:45 +0630

    change social links

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -31,7 +31,7 @@ const SocialLinks = () => {
           Mail <HiOutlineMail size={30} />
         </>
       ),
-      href: "mailto:ayenyeinchanpyae98@gmail.com",
+      href: "mailto:ayenyeinchanpyae.dev@gmail.com",
     },
     {
       id: 4,
